602733ff4fefe3a634ce07bb9e14bd477a001863
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-09-04 15:53:25 -0300

    models

--- src/utils/http-helper.ts
+++ src/utils/http-helper.ts
@@ -1,7 +1,4 @@
-interface HttpResponse {
-    statusCode: number;
-    body: any;
-}
+import { HttpResponse } from "../models/http-response-movel"
 
 export const ok = async (data:any): Promise<HttpResponse> => {
     return{
