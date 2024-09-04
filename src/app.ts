7d23db69c31a0061a3b0528fd072f273f922d64f
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-09-04 14:43:53 -0300

    players controller

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, {Request, Response, json} from "express"
+import getPlayer from "./controllers/players-controller";
 
 function createApp(){
     const app = express()
 
     app.use(json())
     
-    app.get("/", (req:Request, res:Response)=> {
-        res.status(200).json({player: "Messi"})
-    })
+    app.get("/", getPlayer)
 
     return app;
 }
